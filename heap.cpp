643572a72cd80ffc621e8170ecc9da7fc02f4b4c
Yusen <yusenliu@cs.ucsb.edu>
2026-02-25 16:50:48 -0800

    fixed bubble up

--- heap.cpp
+++ heap.cpp
@@ -8,34 +8,24 @@ using std::cout;
 // Pushes a value into the heap, then ensures
 // the heap is correctly arranged
 void Heap::push(int value){
-    vdata.push_back(value);
-    bubble_up(vdata.size() - 1);
+
 }
 
 // Pops the minimum value off the heap
 // (but does not return it), then ensures
 // the heap is correctly arranged
 void Heap::pop(){
-    if (vdata.empty()) return;
-    // move last element to root, remove min
-    vdata[0] = vdata.back();
-    vdata.pop_back();
-    if (!vdata.empty())
-        bubble_down(0);
+
 }
 
 // Returns the minimum element in the heap
 int Heap::top(){
-  if (vdata.empty()){
-      // undefined behaviour in original spec; return 0
-      return 0;
-  }
-  return vdata[0];
+
 }
 
 // Returns true if the heap is empty, false otherwise
 bool Heap::empty(){
-  return vdata.empty();
+
 }
     
 // helper implementations ----------------------------------
@@ -58,6 +48,9 @@ void Heap::bubble_down(int index) {
 }
 
 void Heap::bubble_up(int index) {
+    if (index <= 0) {
+      return;
+    }
     while (index > 0) {
         int parent = (index - 1) / 2;
         if (vdata[index] < vdata[parent]) {
