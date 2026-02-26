1c8c113acde467ad83263c23247ec7828f909d75
Yusen <yusenliu@cs.ucsb.edu>
2026-02-25 17:58:42 -0800

    added heapify again

--- heap.h
+++ heap.h
@@ -11,14 +11,23 @@
 class Heap{
 
  public:
+    Heap() = default;            // default constructor creates empty heap
     void push(int value);     // insert value; //duplicates are allowed
     void pop(); // delete the min element 
     int top();
     bool empty();
     Heap(std::vector<int>::iterator start, std::vector<int>::iterator end) {
-         for (auto it = start; it != end; ++it) {
-            push(*it);
-         }
+        // copy range into internal vector
+        vdata.assign(start, end);
+        // run Floyd's heapify algorithm: bubble down each non-leaf node
+        // starting from the last parent and moving backwards to 0.
+        // note: when vdata.size()==0 or 1 there's nothing to do.
+        if (vdata.size() > 1) {
+            // size_t is unsigned; use a reverse loop that handles zero correctly.
+            for (std::size_t i = vdata.size() / 2; i-- > 0; ) {
+                bubble_down(i);
+            }
+        }
     }
 
     
